125d5c65b9ad685609b08f30e16506022c72ff7e
Gyh592358736 <77520082@qq.com>
2020-08-13 09:30:05 +0000

    邮箱认证

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -21,6 +21,8 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+
+
     ];
 
     /**
@@ -37,6 +39,10 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+
+
+            //开发者添加
+            \App\Http\Middleware\EnsureEmailIsVerified::class, 
         ],
 
         'api' => [
@@ -63,5 +69,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'EnsureEmail'=> \App\Http\Middleware\EnsureEmailIsVerified::class, 
     ];
 }
