03b3e0007e9dc424ac8e6af56960ee468ed5d8be
Gyh592358736 <77520082@qq.com>
2020-08-14 03:55:14 +0000

    上传头像

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests\UserRequest;
 use App\Models\User;
+use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
@@ -16,9 +17,20 @@ class UsersController extends Controller
     	 return view('users.edit', compact('user'));
     }
 
-    public function update(UserRequest $request, User $user)
+    public function update(UserRequest $request,ImageUploadHandler $uploader,User $user)
     {
-        $user->update($request->all());
+    	$data=$request->all();
+    	if($request->avatar){
+            $result = $uploader->save($request->avatar, 'avatars', $user->id);
+            if($result){
+                $data['avatar'] = $result['path'];
+            }else{
+                //上传有错误  withErrors可以携带回错误信
+                return back()->withErrors(['上传图片格式只支持png, jpg, gif, jpeg这四种格式']);
+            }
+        }
+
+        $user->update($data);
         return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
     }
 }
