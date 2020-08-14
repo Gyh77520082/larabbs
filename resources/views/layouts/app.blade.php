a16f354c5cce266aec9615d20dc6cc145a089880
Gyh592358736 <77520082@qq.com>
2020-08-14 09:59:06 +0000

    用户可以上传话题图片

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,8 +8,9 @@
   	<meta name="csrf-token" content="{{ csrf_token() }}">
 	<title>@yield('title','larabbs')-laravel进阶</title>
 	<!-- Styles -->
-  <link href="{{ mix('css/app.css') }}" rel="stylesheet">
-  @yield('styles')
+  	<link href="{{ mix('css/app.css') }}" rel="stylesheet">
+
+  	@yield('styles')
 </head>
 <body>
 	<div id="app" class="{{ route_class() }}-page">
@@ -20,8 +21,9 @@
 	 	</div>
 		@include('layouts._footer')
   	</div>
-  	 <!-- Scripts -->
-  <script src="{{ mix('js/app.js') }}"></script>
-  @yield('scripts')
+  	<!-- Scripts -->
+  	<script src="{{ mix('js/app.js') }}"></script>
+
+  	@yield('scripts')
 </body>
 </html>
