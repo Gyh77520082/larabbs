cdfdffa10a12e4267f3141d92d6c551bcbf92517
Gyh592358736 <77520082@qq.com>
2020-08-14 05:39:21 +0000

    授权访问

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -13,12 +13,15 @@ class UsersController extends Controller
     public function show(User $user){
     	return view('users.show',compact('user'));
     }
+    //修改界面
     public function edit(User $user){
+        $this->authorize('update', $user);
     	 return view('users.edit', compact('user'));
     }
-
+    //更新
     public function update(UserRequest $request,ImageUploadHandler $uploader,User $user)
     {
+        $this->authorize('update', $user);  
     	$data=$request->all();
     	if($request->avatar){
             $result = $uploader->save($request->avatar, 'avatars', $user->id, 416);
@@ -29,5 +32,11 @@ class UsersController extends Controller
 
         $user->update($data);
         return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
+    }
+
+    //授权
+     public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['show']]);
     }
 }
