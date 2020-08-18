c15d9a0e1cb270a07102bd0f831a9d72e67f87d7
Gyh592358736 <77520082@qq.com>
2020-08-18 02:29:39 +0000

    回复数据

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -16,6 +16,11 @@ class Topic extends Model
     {
         return $this->belongsTo(User::class);
     }
+    //多个回复
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
 
 
     public function scopeWithOrder($query,$order){
--- app/Models/User.php
+++ app/Models/User.php
@@ -34,4 +34,10 @@ class User extends Authenticatable implements MustVerifyEmailContract
     {
         return $this->id == $model->user_id;
     }
+
+    //多个评论
+     public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
 }
