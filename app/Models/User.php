2b2a9dbe9fd5b154df5efd725d843cfcf92aa508
Gyh592358736 <77520082@qq.com>
2020-08-18 09:23:03 +0000

    消息通知列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -59,4 +59,12 @@ class User extends Authenticatable implements MustVerifyEmailContract
     {
         return $this->hasMany(Reply::class);
     }
+
+    //清除提示
+     public function markAsRead()
+    {
+        $this->notification_count = 0;
+        $this->save();
+        $this->unreadNotifications->markAsRead();
+    }
 }
